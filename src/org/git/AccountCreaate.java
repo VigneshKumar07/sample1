2939c11554ef2d3a8909dd1e11dbe777506095c4
VigneshKumar07 <madhanvicky71295@gmail.com>
2022-05-21 17:21:26 +0530

    password ok

--- src/org/git/AccountCreaate.java
+++ src/org/git/AccountCreaate.java
@@ -2,4 +2,11 @@ package org.git;
 
 public class AccountCreaate {
 
+	public static void passwordfunctiolaity() {
+		System.out.println("password Functionality ok");
+	}
+	public static void main(String[] args) {
+		
+		passwordfunctiolaity();
+	}
 }
