9e8f9b38d8fb67bf67afb58ffcce867d688216c3
VigneshKumar07 <madhanvicky71295@gmail.com>
2022-05-21 15:40:20 +0530

    username Functionality completed

--- src/org/git/AccountCreaate.java
+++ src/org/git/AccountCreaate.java
@@ -1,5 +1,12 @@
 package org.git;
 
 public class AccountCreaate {
-
+	
+	public static void username() {
+	  System.out.println("username Functionality");
+	}
+	
+	public static void main(String[] args) {
+		username();
+	}
 }
